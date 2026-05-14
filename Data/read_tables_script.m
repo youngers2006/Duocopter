29c4b3b45d52ae006e4eeccd6d34edcb7ceb1d9c
youngers2006 <sy2224@ic.ac.uk>
2026-05-15 00:52:17 +0100

    added changes and started Linear Quadratic Integrator controller

--- Data/read_tables_script.m
+++
@@ -1,2 +0,0 @@
-Large_table = readtable("Large_Motor_data.csv");
-disp(Large_table)
---
+++ Main/setup_sim_PID.m
@@ -0,0 +1,20 @@
+% parameters
+params.g = 9.81;
+params.m_cart = 0.150;
+params.m_total = 0.650;
+params.mu_k = 0.14;
+params.L_thrust = 0.27;
+
+% configurations
+config.velocity_tolerance = 1e-3;
+config.initial_height = 0.0;
+config.max_thrust_gf = 1300;
+config.target_height = 1.0;
+
+% hover thrust
+hover_thrust_gf = (params.m_total * 1000) / 2; 
+exact_throttle = interp1(params.thrust_array, ...
+   params.throttle_array, hover_thrust_gf, 'linear', 'extrapolate');
+hover_throttle = round(exact_throttle);
+
+disp('Workspace loaded.');
